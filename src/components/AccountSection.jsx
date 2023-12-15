740b0a24f61786b1425576eb2a5fc7a52fecb09f
khanh.truong <abby79truong@gmail.com>
2023-12-15 14:22:13 +0100

    update component: AccountSection

--- src/components/AccountSection.jsx
+++ src/components/AccountSection.jsx
@@ -1,9 +1,12 @@
 import '../css/main.css';
 
-const AccountSection = () => {
+const AccountSection = ({ accountTitle, accountAmount, amountDescription, buttonLabel }) => {
   return (
     <section className="account">
-      
+      <h3>{accountTitle}</h3>
+      <p className="account-amount">{accountAmount}</p>
+      <p className="account-description">{amountDescription}</p>
+      <button className="view-transactions-button">{buttonLabel}</button>
     </section>
   );
 };
